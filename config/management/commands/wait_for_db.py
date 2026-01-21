fb4aa7006b38522648281a77c05c886503a51319
IqramRafie <iqram.mohdrafie@gmail.com>
2026-01-21 20:44:49 +0800

    Fix bug for Docker

--- config/management/commands/wait_for_db.py
+++ config/management/commands/wait_for_db.py
@@ -1,17 +1,24 @@
 import time
+import logging
 from django.core.management.base import BaseCommand
 from django.db import connections
 from django.db.utils import OperationalError
 
+logger = logging.getLogger(__name__)
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        logger.info('Starting wait_for_db command')
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
             try:
                 db_conn = connections['default']
                 db_conn.cursor()
-            except OperationalError:
+                logger.info('Database connection successful')
+            except OperationalError as e:
+                logger.warning(f'Database unavailable: {e}, waiting 1 second...')
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
         self.stdout.write(self.style.SUCCESS('Database available!'))
+        logger.info('Database is now available')
